72f6df90c6b71693b0b94663020657daf6dada14
Jonah G <122639343+JonahG046@users.noreply.github.com>
2025-02-12 12:06:09 -0600

    Update app.py

--- RWapp/app.py
+++ RWapp/app.py
@@ -156,6 +156,6 @@ def logout():
   return redirect(url_for('home'))
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000)
+  app.run(host='0.0.0.0', port=5000)
   #To show the footer in the template
   foot = True
